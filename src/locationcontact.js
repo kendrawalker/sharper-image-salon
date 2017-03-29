20116d6dc32f99cc78e794bec0b6a7f9e22fb7d7
Kendra Walker <kendra.danielle.walker@gmail.com>
2017-03-29 16:03:40 +0200

    fixed location image

--- src/locationcontact.js
+++ src/locationcontact.js
@@ -19,7 +19,7 @@ export function LocationContact (props) {
                         <h2>Find us Here!</h2>
                         <h3>209 S. Main Street, Batesville, IN 47006</h3>
                         <a href="https://goo.gl/maps/xehxumSJFKH2">
-                            <img id="location-map" src="/sharperimagemaplocation.jpg"/>
+                            <img id="location-map" src="/images/sharperimagemaplocation.jpg"/>
                         </a>
                     </div>
                 </div>
